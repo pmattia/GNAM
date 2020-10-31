1e84dcaf643031c13c1a72688ce4ebefb5d6de13
nerdyduck <mattia.guglielmo@gmail.com>
2020-10-31 20:45:49 +0100

    wip rush

---
+++ Assets/Scripts/Eatable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eatable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision " + collision.gameObject.name);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger " + other.gameObject.name);
+        if(other.gameObject.name == "CenterEyeAnchor")
+            Destroy(gameObject);
+    }
+}
